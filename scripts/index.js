7045616eab923605df9adf112f5b5a4338214a68
Dana Kun <idealdl@yandex.ru>
2022-09-13 12:49:48 +0200

    fix popup classes

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,13 @@ const popupOverlay = document.querySelector('.popup');
 const openPopupButton = document.querySelector('.profile__edit-button');
 const closePopupButton = document.querySelector('.popup__close');
 
-const popupForm = document.querySelector('.popup-window__form');
+const popupForm = document.querySelector('.popup__form');
 //const saveButton = document.querySelector('.popup-window__save-button');
 
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let newName = document.querySelector('.popup-window__input_type_name');
-let newJob = document.querySelector('.popup-window__input_type_job')
+let newName = document.querySelector('.popup__input_type_name');
+let newJob = document.querySelector('.popup__input_type_job')
 
 
 function takeInfo () {
