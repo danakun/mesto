53fce11504d5e46138d862811629acc50c413dec
Dana Kun <idealdl@yandex.ru>
2022-11-10 10:19:06 +0100

    names change

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 
-import { Card } from './card.js'
-import { FormValidator } from './formvalidator.js'
+import { Card } from './Card.js'
+import { FormValidator } from './FormValidator.js'
 import { initialCards } from './cards.js';
 
 const validationObject = {
