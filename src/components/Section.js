39d596a043bfe3503a7eb8dcde404bdb635069a2
Dana Kun <dana.liepinsh@gmail.com>
2022-12-16 09:30:26 +0100

    delete extra spaces

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,11 +4,13 @@ this._items = items;
 this._renderer = renderer;
 this.container =  document.querySelector(containerSelector);
   }
+
   addItem(element) {
     //попадает готовая разметка
 //публичный метод принимает элемент и добавляет его в разметку
 this.container.prepend(element);
   }
+
   renderItems() {
     //публичный метод проходит циклом forEach по всем (initial) items и рендерит элементы
     // использует renderer
